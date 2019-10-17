33aad4a28070b589e63f83d85d4c87ee0344bae0
Arias macarena <ariasmaca55@gmail.com>
2019-10-17 14:00:47 -0300

    Modificando el models.py
    Agregue las clases turno y seguimiento en el archivo models.py.
    Tambien cambie el dioma en el setting.py.

--- autoridades/models.py
+++ autoridades/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+class Turno(models.Model):
+    TURNOS_CHOICES = (
+    	('M', 'Mañana'),
+        ('T', 'Tarde'),
+        ('N', 'Noche'),
+    )
+    turnos = models.CharField(
+        max_length=1,
+        choices=TURNOS_CHOICES
+    )
+
+    def __str__(self):
+    	return '%s' % (self.turnos)
+
+
 class Profesor(models.Model):
 	dni = models.PositiveSmallIntegerField(
 		primary_key=True,
@@ -18,7 +33,8 @@ class Profesor(models.Model):
 
 	def __str__(self):
 		return '%s' % (self.nombre)
-# Create your models here.
+
+
 class Asignatura(models.Model):
 	codigo = models.PositiveIntegerField('Código asignatura', 
 		unique=True,
@@ -39,6 +55,36 @@ class Asignatura(models.Model):
 
 	def __str__(self):
 		return '%s %s' % (self.nombre, self.año)
+
+	class Meta:
+		verbose_name_plural = 'asignaturas'
+
+
+class Seguimiento(models.Model):
+	fecha = models.DateField('Fecha actual', help_text='Ingrese la fecha actual')
+	profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, help_text = 'Elegir un profesor')
+	turno = models.ForeignKey(Turno, on_delete=models.CASCADE, help_text = 'Elegir un turno')
+	curso = models.PositiveIntegerField('Curso perteneciente',
+		validators=[
+			MaxValueValidator(6),
+			MinValueValidator(1)
+		],
+		help_text='Ingrese el curso correspondiente.'
+	)
+	division = models.PositiveIntegerField('Division del curso',
+		validators=[
+			MaxValueValidator(7),
+			MinValueValidator(1)
+		],
+		help_text='Ingrese la division del curso correspondiente.'
+	)
+	asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE, help_text = 'Elegir una asignatura')
+	ausente = models.BooleanField(blank=True, null=True)
+	tarde = models.BooleanField(blank=True, null=True)
+
+
+	def __str__(self):
+		return '%s %s %s %s %s %s' % (self.fecha, self.profesor, self.turno, self.curso, self.division, self.asignatura)
 
 	class Meta:
 		verbose_name_plural = 'asignaturas'

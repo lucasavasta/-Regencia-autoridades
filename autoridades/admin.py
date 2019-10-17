f233a1a30f8e7cd1d371fd6a3d668b57da01004d
luca <lucasavasta@hotmail.com>
2019-10-17 14:02:58 -0300

    modificacion del admin

--- autoridades/admin.py
+++ autoridades/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import (
 	Profesor, Asignatura
 )
+@admin.register(Profesor)
+class ProfesorAdmin(admin.ModelAdmin):
+	list_filter = ('dni', 'nombre')
 
+@admin.register(Asignatura)
+class AsignaturaAdmin(admin.ModelAdmin):
+	list_filter = ['codigo']
